5d84cf669d47392205bd6949dc2356f8093c227c
aiquant4 <sumit.bera@aiquantgroup.com>
2025-09-17 16:30:35 +0530

    staging branch added

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
-  //  base: '/5am.earth/',
+  // base:"/",
+   base: '/5am.earth/',
   server: {
     allowedHosts: true
   },
