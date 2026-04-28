bee12e532b2f8315aca20a9d8a6f6262aace894a
Aditya Singh Solanki <aditya.solanki@aiquantgroup.com>
2026-04-28 11:37:53 +0530

    update to v2

--- scripts/copy404.js
+++
@@ -1,8 +0,0 @@
-import { cpSync } from 'fs'
-try {
-  cpSync('dist/index.html', 'dist/404.html')
-  console.log('Copied dist/index.html -> dist/404.html')
-} catch (e) {
-  console.error('Failed to create 404.html', e)
-  process.exit(1)
-}
